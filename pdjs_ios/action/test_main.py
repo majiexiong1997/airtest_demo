dcef4a1335553576081c53762331e383d66fab3b
majiexiong <1455080341@qq.com>
2020-08-12 14:10:47 +0800

    airtest

---
+++ pdjs_ios/action/test_main.py
@@ -0,0 +1,12 @@
+
+
+from pdjs_ios.module.main import *
+
+class TestMain:
+    def setup(self):
+        self.main = Main()
+
+
+
+    def teardown(self):
+        pass
---
+++ pdjs_ios/module/main.py
@@ -0,0 +1,9 @@
+
+from pdjs_ios.center.pdjs_main_page import PdjsMainPage
+
+class Main(PdjsMainPage):
+    def login(self):
+        '''
+        登录
+        :return:
+        '''
